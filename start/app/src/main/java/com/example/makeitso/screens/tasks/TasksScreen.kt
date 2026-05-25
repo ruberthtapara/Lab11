985588dfbcf15668bca08820d39b5fc542593311
ruberthtapara <ruberth.tapara@tecsup.edu.pe>
2026-05-25 11:55:26 -0500

    implementacion para mostrar cambios en el interfaz como para subir al firebase

--- start/app/src/main/java/com/example/makeitso/screens/tasks/TasksScreen.kt
+++ start/app/src/main/java/com/example/makeitso/screens/tasks/TasksScreen.kt
@@ -36,13 +36,18 @@ import com.example.makeitso.common.ext.toolbarActions
 import com.example.makeitso.model.Task
 import com.example.makeitso.theme.MakeItSoTheme
 
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
 @Composable
 @ExperimentalMaterialApi
 fun TasksScreen(
   openScreen: (String) -> Unit,
   viewModel: TasksViewModel = hiltViewModel()
 ) {
+  val tasks = viewModel.tasks.collectAsStateWithLifecycle(emptyList())
+
   TasksScreenContent(
+    tasks = tasks,
     onAddClick = viewModel::onAddClick,
     onSettingsClick = viewModel::onSettingsClick,
     onTaskCheckChange = viewModel::onTaskCheckChange,
@@ -58,6 +63,7 @@ fun TasksScreen(
 @ExperimentalMaterialApi
 fun TasksScreenContent(
   modifier: Modifier = Modifier,
+  tasks: State<List<Task>>,
   onAddClick: ((String) -> Unit) -> Unit,
   onSettingsClick: ((String) -> Unit) -> Unit,
   onTaskCheckChange: (Task) -> Unit,
@@ -87,7 +93,7 @@ fun TasksScreenContent(
       Spacer(modifier = Modifier.smallSpacer())
 
       LazyColumn {
-        items(emptyList<Task>(), key = { it.id }) { taskItem ->
+        items(tasks.value, key = { it.id }) { taskItem ->
           TaskItem(
             task = taskItem,
             options = listOf(),
@@ -104,8 +110,11 @@ fun TasksScreenContent(
 @ExperimentalMaterialApi
 @Composable
 fun TasksScreenPreview() {
+  val tasks = remember { mutableStateOf(emptyList<Task>()) }
+
   MakeItSoTheme {
     TasksScreenContent(
+      tasks = tasks,
       onAddClick = { },
       onSettingsClick = { },
       onTaskCheckChange = { },
